2c0a3e22fc396bf409fa8496c718550a0c8d4f20
Unnimaya T <Unnimaya.T@godigit.com>
2024-08-21 12:11:40 +0530

    setter-injection

--- SpringXML/src/main/java/com/example/SpringXML/Car.java
+++ SpringXML/src/main/java/com/example/SpringXML/Car.java
@@ -1,11 +1,23 @@
 package com.example.SpringXML;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
 public class Car {
     private  Engine engine;
 
-    public Car(Engine engine){
-        this.engine=engine;
+    public Car(){}
+
+    public Car(Engine engine) {
+        this.engine = engine;
     }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+
     public void drive(){
         engine.start();
         System.out.println("Car is driving...");
--- SpringXML/src/main/java/com/example/SpringXML/Engine.java
+++ SpringXML/src/main/java/com/example/SpringXML/Engine.java
@@ -1,6 +1,18 @@
 package com.example.SpringXML;
 
 public class Engine {
+    private String type;
+
+    public Engine(){}
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public void start(){
         System.out.println("Engine started!!");
     }
